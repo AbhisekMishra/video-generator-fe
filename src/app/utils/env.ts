37ad6c08bfc7565103f3cf996b7344f41e196ae7
Abhisek <abhisekmishra55@gmail.com>
2025-12-21 05:53:45 +0400

    Delete src directory

--- src/app/utils/env.ts
+++
@@ -1,6 +0,0 @@
-export const env = {
-    supabase: {
-        anonKey: process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlha2h5aGlmY3RjbmthamdncGd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0MTAzODEsImV4cCI6MjA1ODk4NjM4MX0.Occ7ter7lms8ZrwmbcigVFQ6eHPISE8RkzivaEnI9Ns',
-        url: process.env.SUPABASE_URL || 'https://iakhyhifctcnkajggpgy.supabase.co',
-    }
-}
